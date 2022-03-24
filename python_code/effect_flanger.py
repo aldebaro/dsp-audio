1c9b1879bbd3594495571470f50b3c796d7ea145
1176325 <1176325@gitlab.lasse.ufpa.br>
2022-03-24 11:45:24 -0300

    Initial code for audio effects

---
+++ python_code/effect_flanger.py
@@ -0,0 +1,12 @@
+'''
+Demonstrates specific audio effect using digital signal 
+processing (DSP).
+This effect is called XXX
+References:
+Author: 
+Date: 
+'''
+
+def effect_XXX():
+    #TO-DO
+    return None
