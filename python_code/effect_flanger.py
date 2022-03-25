1f5507d6cf34ba0cccaac73b717e2d5d233197d0
marcocolla2407 <102337129+marcocolla2407@users.noreply.github.com>
2022-03-25 19:07:19 -0300

    inicio código

--- python_code/effect_flanger.py
+++ python_code/effect_flanger.py
@@ -4,9 +4,29 @@ processing (DSP).
 This effect is called XXX
 References:
 Author: Marco Aurelio
-Date: 
+Date: 25/03/2022
 '''
 
-def flanger():
-    #TO-DO
-    return None
+import numpy as np
+def flanger(audioin,Fs):
+    length= len(audioin)
+    nsample= np.array(range(length))
+    
+    lfo_freq=1/2 #LFO Freq (Hz)
+    lfo_amp=0.008 #LFO Amp (sec)
+    lfo=2+signal.sawtooth(2*np.pi*lfo_freq*(nsample)/Fs,0.5) #Generate triangle wave
+    
+    plt.plot(lfo)
+    plt.xlabel ("Sample Index")
+    plt.ylabel("Delay Amount (normalized)")
+    plt.title("Delay vs Time")
+    
+    index= np.around[nsample-Fs*lfo_amp*lfo]
+    index[index<0]= 0 #Clip delay
+    index[index>(length-1)]= length-1
+    
+    out= np.zeros(length) #Imput Signal
+    print(out.shape)
+    for j in range(length): #For each sample
+       out[j]= np.float(audioin[j]) + np.float(audioin[int(index[j])]) #Add Delayed signal
+    return out
