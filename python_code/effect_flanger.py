56029cb62179a30ab18f703f27bd32c2fbee139b
marcocolla2407 <102337129+marcocolla2407@users.noreply.github.com>
2022-03-26 20:13:01 -0300

    ajustes parâmetros

--- python_code/effect_flanger.py
+++ python_code/effect_flanger.py
@@ -6,22 +6,16 @@ References:
 Author: Marco Aurelio
 Date: 25/03/2022
 '''
-
 import numpy as np
 def flanger(audioin,Fs):
     length= len(audioin)
     nsample= np.array(range(length))
     
-    lfo_freq=1/2 #LFO Freq (Hz)
-    lfo_amp=0.008 #LFO Amp (sec)
-    lfo=2+signal.sawtooth(2*np.pi*lfo_freq*(nsample)/Fs,0.5) #Generate triangle wave
+    lfo_freq=0.1 #LFO Freq (Hz)
+    lfo_amp=0.1 #LFO Amp (sec)
+    lfo=np.sin(2*np.pi*lfo_freq*(nsample)/Fs) #Generate triangle wave
     
-    plt.plot(lfo)
-    plt.xlabel ("Sample Index")
-    plt.ylabel("Delay Amount (normalized)")
-    plt.title("Delay vs Time")
-    
-    index= np.around[nsample-Fs*lfo_amp*lfo]
+    index= np.round(nsample-Fs*lfo_amp*lfo)
     index[index<0]= 0 #Clip delay
     index[index>(length-1)]= length-1
     
@@ -29,4 +23,8 @@ def flanger(audioin,Fs):
     print(out.shape)
     for j in range(length): #For each sample
        out[j]= np.float(audioin[j]) + np.float(audioin[int(index[j])]) #Add Delayed signal
+    plt.plot(out,'r',audioin,'b')
+    plt.xlabel ("Sample Index")
+    plt.ylabel("Delay Amount (normalized)")
+    plt.title("Delay vs Time")
     return out
