0712f04f414d0470f76d4a9266fea3f9dc99c3a3
fredhrl <31097759+fredhrl@users.noreply.github.com>
2022-03-28 09:58:56 -0300

    updated effects_chorus,py
    added my own code to peform the chorus effect

--- python_code/effect_chorus.py
+++ python_code/effect_chorus.py
@@ -1,50 +1,30 @@
-'''
-Demonstrates specific audio effect using digital signal 
-processing (DSP).
-This effect is called XXX
-References:
-Author: 
-Date: 
-'''
-from numpy import random
 import numpy as np
-import copy
 
-def chorus(sig, ts, level: float=0.1, rate: float=5000, LFO: bool=True):
-        """
-        Function to replicate chorus effect on a signal.
-        The chorus idea is to simulate the feeling of an ensemble of sounds, that
-        is the same sequence of sound being heard with slight variations either in
-        tone and/or time (small delays, usually 10-50ms), such that it sounds like
-        multiple and different sources are producing the same notes. This "randomness"
-        can be done digitally by using a sinusoid that represents a low frequency
-        oscillator or a random number generator to vary the delay values between
-        two extremes and apply it multiple times with small offsets between each
-        application.
-        
-        :level: level of the effect to be applied
-        :rate: frequency of the LFO in Hz
-        """
-        fs=1.0/ts
-        #sig = self.normalize(self.sig)
-        t = np.arange(0, len(sig)*ts, ts) # time array
-        delayMin = 10e-3 # ms
-        delayMax = 25*1e-3 # ms
-        delayMean = (delayMin + delayMax)/2
-        if LFO: 
-            lfo = delayMean*np.sin(2*np.pi*t*(rate/fs)) + delayMean
-            lfo *= fs # from time to samples
-            idxs = np.arange(len(sig)) # array of indices
-            currDelay = np.int64(np.round(idxs - lfo)) # assure the integer values
-        else:
-            # Uniform distrution random number generator
-            rnd = random.uniform(delayMin, delayMax/2, len(sig))
-            rnd *= fs
-            idxs = np.arange(len(sig)) # array of indices
-            currDelay = np.int64(np.round(idxs - rnd)) # assure the integer values
-        newsignal = copy.deepcopy(sig) #deep, instead of shallow copy
-        # Adding delayed copies of the signal
-        for i in np.arange(0, 4*ts, 2*ts,dtype=np.int64):
-            newsignal += level*(sig[currDelay - i])
-        #sig = self.denormalize(sig)
-        return newsignal
+def chorus(signal,ts,rate = 5000,level = 0.5):
+  """
+  Function to aplly a chorus effect to a signal. this effect adds
+  several delayed copies of the original signal(within the 10-25 ms range)
+  using a low frequency oscilator(lfo), function takes the original signal
+  and it's time period and returns the signal with chorus applied.
+  """
+  fs=1.0/ts # signal frequency
+  lfo_rate = rate # sine frequency(Hz)
+  copy_level = level # effects' amplification
+
+  t = np.arange(0, len(signal)*ts, ts) # 
+
+  #recomended delay is usually defined in a interval between 10 and 25 ms
+  min_delay = 0.010
+  max_delay = 0.025
+  avg_delay = (min_delay + max_delay)/2 
+
+  lfo = (avg_delay*np.sin(2*np.pi*t*(lfo_rate/fs)) + avg_delay)*fs #LFO
+
+  inds = np.arange(len(signal)) #array of indices to apply the delay 
+  delay = np.int64(np.ceil(inds - lfo))
+
+  chorus = signal #deep, instead of shallow copy
+  # Adding delayed copies of the signal
+  for i in np.arange(0, 4*ts, 2*ts,dtype=np.int64):
+      chorus += copy_level*(signal[delay - i])# sums multiple delayed copies of the original signal
+  return chorus
