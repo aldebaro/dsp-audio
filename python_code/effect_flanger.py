d0af84cf4ad96115b2169a5703ed0e95fb1d263c
1176325 <1176325@gitlab.lasse.ufpa.br>
2022-03-28 12:55:33 -0300

    Remove plt calls from function

--- python_code/effect_flanger.py
+++ python_code/effect_flanger.py
@@ -7,6 +7,7 @@ Author: Marco Aurelio Colla
 Date: 27/03/2022
 '''
 import numpy as np
+
 def flanger(audioin,Fs):
     length= len(audioin)
     nsample= np.array(range(length))
@@ -23,10 +24,11 @@ def flanger(audioin,Fs):
     print(out.shape)
     for j in range(length): #For each sample
        out[j]= np.float(audioin[j]) + np.float(audioin[int(index[j])]) #Add Delayed signal
-    plt.plot(out,'r',audioin,'b')
-    plt.xlabel ("Sample Index")
-    plt.ylabel("Delay Amount (normalized)")
-    plt.title("Delay vs Time")
+    if False:
+        plt.plot(out,'r',audioin,'b')
+        plt.xlabel ("Sample Index")
+        plt.ylabel("Delay Amount (normalized)")
+        plt.title("Delay vs Time")
     return out
 
 if False:    
